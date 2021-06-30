c59247dd5b460f36c0084f176248d50658caa225
mieumje <66072832+mieumje@users.noreply.github.com>
2021-06-30 18:34:26 +0900

    수정/조회 기능 API 생성 및 테스트 코드 작성

--- src/main/java/com/mieumje/springboot/service/PostsService.java
+++ src/main/java/com/mieumje/springboot/service/PostsService.java
@@ -1,7 +1,10 @@
 package com.mieumje.springboot.service;
 
+import com.mieumje.springboot.domain.posts.Posts;
 import com.mieumje.springboot.domain.posts.PostsRepository;
+import com.mieumje.springboot.web.dto.PostsResponseDto;
 import com.mieumje.springboot.web.dto.PostsSaveRequestDto;
+import com.mieumje.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,9 +13,26 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class PostsService {
     private final PostsRepository postsRepository;
-
+    // 등록 기능
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+    
+    // 수정/조회 기능
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
