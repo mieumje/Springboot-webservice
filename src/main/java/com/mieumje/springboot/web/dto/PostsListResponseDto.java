bfd218c34d2718c0c1ee9ae60d01b6723176f79d
mieumje <66072832+mieumje@users.noreply.github.com>
2021-07-13 15:17:37 +0900

    게시글 전체 조회하기

---
+++ src/main/java/com/mieumje/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,22 @@
+package com.mieumje.springboot.web.dto;
+
+
+import com.mieumje.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
