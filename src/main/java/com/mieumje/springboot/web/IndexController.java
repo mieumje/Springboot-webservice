fc6aa5a1855c76ccc5c500732bd7b8bb47511450
mieumje <66072832+mieumje@users.noreply.github.com>
2021-07-13 15:49:56 +0900

    게시글 수정하기

--- src/main/java/com/mieumje/springboot/web/IndexController.java
+++ src/main/java/com/mieumje/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.mieumje.springboot.web;
 
 import com.mieumje.springboot.service.PostsService;
+import com.mieumje.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -18,9 +20,18 @@ public class IndexController {
         model.addAttribute("posts",postsService.findAllDesc());
         return "index";
     }
-
+    // 글 저장
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    // 글 수정
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+
+        return "posts-update";
     }
 }
