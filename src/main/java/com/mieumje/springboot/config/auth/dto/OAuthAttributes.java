cb0f1270ea2bf7dde018a0e4fd372a64b62265e4
mieumje <66072832+mieumje@users.noreply.github.com>
2021-07-20 19:01:11 +0900

    구글 로그인 테스트(성공)
    properties 파일 이름 변경 필요

--- src/main/java/com/mieumje/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/mieumje/springboot/config/auth/dto/OAuthAttributes.java
@@ -16,7 +16,7 @@ public class OAuthAttributes {
     private String picture;
 
     @Builder
-    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture){
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
         this.attributes = attributes;
         this.nameAttributeKey = nameAttributeKey;
         this.name = name;
@@ -24,11 +24,7 @@ public class OAuthAttributes {
         this.picture = picture;
     }
 
-    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
-        return ofGoogle(userNameAttributeName, attributes);
-    }
-
-    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes){
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String,Object> attributes){
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
@@ -38,7 +34,18 @@ public class OAuthAttributes {
                 .build();
     }
 
-    public User toEntity(){
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+
+    public User toEntity() {
         return User.builder()
                 .name(name)
                 .email(email)
--- src/main/java/com/mieumje/springboot/web/IndexController.java
+++ src/main/java/com/mieumje/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.mieumje.springboot.web;
 
+import com.mieumje.springboot.config.auth.dto.SessionUser;
 import com.mieumje.springboot.service.PostsService;
 import com.mieumje.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +9,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model)
     {
         model.addAttribute("posts",postsService.findAllDesc());
+        // userName을 사용할 수 있게 model에 저장
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
     }
     // 글 저장
@@ -34,4 +43,5 @@ public class IndexController {
 
         return "posts-update";
     }
+
 }
